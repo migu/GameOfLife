b7f595537744746ea4b9137aa685fce7abd4fdcc
Michael Gutbier <dev@michael-gutbier.de>
2011-12-02 01:11:58 +0100

    new: slider for speed control, start/stop button

--- src/main/java/GameOfLife.java
+++ src/main/java/GameOfLife.java
@@ -1,5 +1,4 @@
 import java.awt.BorderLayout;
-import java.awt.Button;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
@@ -9,9 +8,14 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JSlider;
 import javax.swing.SwingUtilities;
+import javax.swing.Timer;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,17 +23,29 @@ import org.slf4j.LoggerFactory;
 public class GameOfLife extends JFrame {
     private static final long serialVersionUID = -4365934143909541035L;
     private static final Logger log = LoggerFactory.getLogger(GameOfLife.class.getSimpleName());
-    private static Color ALIVE = Color.WHITE;
-    private static Color DEAD = Color.LIGHT_GRAY;
-    private static int ROWS = 25;
-    private static int COLS = 25;
+    private static Color ALIVE = Color.BLACK;
+    private static Color DEAD = Color.WHITE;
+    private static int ROWS = 30;
+    private static int COLS = 50;
 
     private Label[][] grid = new Label[ROWS][COLS];
+    private Timer timer = new Timer(1000, new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+            performStep();
+        }
+    });
 
     public GameOfLife() {
         super(GameOfLife.class.getSimpleName());
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
+        final JSlider slider = new JSlider(0, 1000);
+        slider.addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent e) {
+                timer.setDelay(slider.getValue());
+            }
+        });
+        add(slider, BorderLayout.NORTH);
         add(newGrid(), BorderLayout.CENTER);
         add(newGoButton(), BorderLayout.SOUTH);
 
@@ -54,11 +70,17 @@ public class GameOfLife extends JFrame {
         return panel;
     }
 
-    private Button newGoButton() {
-        Button button = new Button("Go");
+    private JButton newGoButton() {
+        final JButton button = new JButton("Go");
         button.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                performStep();
+                if (timer.isRunning()) {
+                    timer.stop();
+                    button.setText("Go");
+                } else {
+                    timer.start();
+                    button.setText("Stop");
+                }
             }
         });
         return button;
@@ -83,19 +105,19 @@ public class GameOfLife extends JFrame {
     }
 
     private void performStep() {
-        log.debug("\ncalculating");
+        log.trace("\ncalculating");
         int[][] neighbours = new int[ROWS][COLS];
         for (int row = 0; row < ROWS; row++) {
             for (int col = 0; col < COLS; col++) {
                 if (grid[row][col].getBackground() == ALIVE) {
-                    log.debug("[{},{}] is alive", row, col);
+                    log.trace("[{},{}] is alive", row, col);
                     for (int rowNeighbour = row - 1; rowNeighbour <= row + 1; rowNeighbour++) {
                         for (int colNeighbour = col - 1; colNeighbour <= col + 1; colNeighbour++) {
                             if (rowNeighbour != row || colNeighbour != col) {
                                 int r = rowNeighbour < 0 ? ROWS - 1 : (rowNeighbour < ROWS ? rowNeighbour : 0);
                                 int c = colNeighbour < 0 ? COLS - 1 : (colNeighbour < COLS ? colNeighbour : 0);
                                 neighbours[r][c]++;
-                                log.debug("increment neighbour count [{},{}] to {}", new Object[] { r, c,
+                                log.trace("increment neighbour count [{},{}] to {}", new Object[] { r, c,
                                         neighbours[r][c] });
                             }
                         }
@@ -108,14 +130,14 @@ public class GameOfLife extends JFrame {
                 int numNeighbors = neighbours[row][col];
                 if (grid[row][col].getBackground() == ALIVE) {
                     if (numNeighbors < 2 || numNeighbors > 3) {
-                        log.debug("[{},{}] dies", row, col);
+                        log.trace("[{},{}] dies", row, col);
                         grid[row][col].setBackground(DEAD);
                     } else {
-                        log.debug("[{},{}] survives", row, col);
+                        log.trace("[{},{}] survives", row, col);
                     }
                 } else {
                     if (numNeighbors == 3) {
-                        log.debug("[{},{}] is born", row, col);
+                        log.trace("[{},{}] is born", row, col);
                         grid[row][col].setBackground(ALIVE);
                     }
                 }
@@ -124,7 +146,7 @@ public class GameOfLife extends JFrame {
     }
 
     public static void main(String[] arg) {
-        log.info("starting application");
+        log.debug("starting application");
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 new GameOfLife();
